4856cfa8924fb24b4021669794ccd299bccb85e1
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-03-20 19:12:27 +0000

    Show student and cats list on index

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cats.models import Student, Cat
 
 def index(request):
+    student_list = Student.objects.order_by('surname')
+    cats_list = Cat.objects.all()
     context_dict = {}
     context_dict['title'] = "The students and their cats are:"
+    context_dict['students'] = student_list
+    context_dict['cats'] = cats_list
     response = render(request, 'cats/index.html', context=context_dict)
     return response
 
