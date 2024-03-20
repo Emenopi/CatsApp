f4bf16992e6428670c4487c291bb063e73c9b723
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-03-20 18:49:55 +0000

    implement models and population script

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    DEFAULT_MAX_LEN = 128
+    forename = models.CharField(max_length=DEFAULT_MAX_LEN)
+    surname = models.CharField(max_length=DEFAULT_MAX_LEN)
+    numCats = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.forename + " " + self.surname
+    
+class Cat(models.Model):
+    DEFAULT_MAX_LEN = 128
+    name = models.CharField(max_length=DEFAULT_MAX_LEN)
+    age = models.IntegerField
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    owner = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.catID = self.name+self.owner.forename
+        super(Cat, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
---
+++ populate_CatsApp.py
@@ -0,0 +1,92 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'CatsApp.settings')
+
+import django
+django.setup()
+from cats.models import Student, Cat
+
+def populate():
+    dannielleCats = [
+        {
+            'name': 'Khepri',
+            'age': 14,
+        },
+        {
+            'name': 'Pandora',
+            'age': 8,
+        },
+        {
+            'name': 'Khaleesi',
+            'age': 10,
+        },
+    ]
+
+    taylorCats = [
+        {
+            'name': 'Meredith Gray',
+            'age': 14
+        },
+        {
+            'name': 'Olivia Benson',
+            'age': 10
+        },
+        {
+            'name': 'Benjamin Button',
+            'age': 6
+        },
+    ]
+
+    cameronCats = [
+        {
+            'name': 'Little Man',
+            'age': 5
+        }
+    ]
+
+    mileyCats = [
+        {
+            'name': 'Lilo',
+            'age': 9
+        },
+        {
+            'name': 'Kiki',
+            'age': 9
+        },
+        {
+            'name': 'Harlem',
+            'age': 8
+        }
+    ]
+
+    students = {'Dannielle': {'cats': dannielleCats, 'surname': 'Spears', 'numCats': 3},
+            'Taylor': {'cats': taylorCats, 'surname': 'Swift', 'numCats': 3},
+            'Cameron': {'cats': cameronCats, 'surname': 'Diaz', 'numCats': 1},
+            'Miley': {'cats': mileyCats, 'surname': 'Cyrus', 'numCats': 3}}
+
+    for student, student_data in students.items():
+        s = add_student(student, student_data['surname'], student_data['numCats'])
+        for cat in student_data['cats']:
+            add_cat(cat['name'], cat['age'], s)
+
+    for s in Student.objects.all():
+        for c in Cat.objects.filter(owner=s):
+            print(f'- {s}: {c}')
+
+def add_student(forename, surname, numCats):
+    s = Student.objects.get_or_create(forename=forename, surname=surname)[0]
+    s.numCats = numCats
+    s.save()
+    return s
+
+def add_cat(name, age, owner):
+    c = Cat.objects.get_or_create(name=name, owner=owner)[0]
+    c.name = name
+    c.age = age
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print('Starting Cat population script...')
+    populate()
+
